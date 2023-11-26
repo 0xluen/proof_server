608456122267ae9fe5422bf31c7683dc9bc0b5df
#Luen <luen.eth@gmail.com>
2023-11-26 19:44:20 +0000

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -21,6 +21,8 @@ contract Bridge {
     uint256 public currentId = 1;
     uint256 public bridgedId = 1;
     uint256 public control = 1;
+    uint256 public bridgeFee = 0.003 ether ; 
+
     bool private locked = false; // Reentry koruması için eklediğimiz kilitleme durumu
 
     modifier noReentry() {
@@ -42,6 +44,16 @@ contract Bridge {
         uint256 amount;
     }
 
+    struct UserDeposit {
+        uint256 lastDepositTime;
+        uint256 dailyTotal;
+    }
+
+    mapping(address => UserDeposit) private userDeposits;
+
+    uint256 public constant MAX_DAILY_DEPOSIT = 1000 * 10**18; // 1000 token, 18 ondalık basamak varsayılarak
+    uint256 public constant DEPOSIT_INTERVAL = 24 hours;
+
     mapping(uint256 => Deposit) public deposits;
     mapping(uint256 => Withdrawal) public withdrawals;
 
@@ -60,6 +72,18 @@ contract Bridge {
         merkleRoot = _merkleRoot;
     }
     
+    function setMerkleRoot(bytes32 newMerkleRoot) external onlyOwner {
+        merkleRoot = newMerkleRoot;
+    }
+
+    function setBridgeFee(uint256 newFee) external onlyOwner {
+        bridgeFee = newFee;
+    }
+
+    function getBridgeFee() external view returns (uint256) {
+        return bridgeFee;
+    }
+
     function setControl(uint256 _control) public onlyOwner {
         control = _control;
     }
@@ -72,10 +96,20 @@ contract Bridge {
         return withdrawals[_id];
     }
 
-    function deposit(uint256 amount,bytes32[] calldata merkleProof) external noReentry {
+    function deposit(uint256 amount,bytes32[] calldata merkleProof) external payable noReentry {
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(msg.value >= bridgeFee , "Fee is too low");
         if (control != 0) {
-            require(verifyMerkleProof(merkleProof, merkleRoot, leaf), "Invalid proof");
+        require(amount <= MAX_DAILY_DEPOSIT, "Exceeds daily limit");
+        UserDeposit storage userDeposit = userDeposits[msg.sender];
+        uint256 timeSinceLastDeposit = block.timestamp - userDeposit.lastDepositTime;
+        if (timeSinceLastDeposit >= DEPOSIT_INTERVAL) {
+            userDeposit.dailyTotal = 0;
+        }
+        require(userDeposit.dailyTotal + amount <= MAX_DAILY_DEPOSIT, "Daily limit exceeded");
+        userDeposit.dailyTotal += amount;
+        userDeposit.lastDepositTime = block.timestamp;
+        require(verifyMerkleProof(merkleProof, merkleRoot, leaf), "Invalid proof");
         }
         require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
         deposits[currentId] = Deposit(msg.sender, amount, block.timestamp, false);
@@ -100,6 +134,7 @@ contract Bridge {
         uint256 balance = token.balanceOf(address(this));
         token.transfer(owner, balance);
     }
+
     function emergencyWith(IERC20 _token) external onlyOwner noReentry {
         uint256 balance = _token.balanceOf(address(this));
         _token.transfer(owner, balance);
